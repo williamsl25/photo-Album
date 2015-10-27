ce2e265d454c2454930177b44b320991a06961ce
Lindsay Williams <williamsl25@yahoo.com>
2015-10-27 14:49:14 -0400

    added a home btn that will return to album page after you are in an album and now able to click the img to open the album

--- main.js
+++ main.js
@@ -14,11 +14,28 @@ var albumTemplate = _.template($('#albumTmpl').html());
   console.log(albumHTML);
   $('.allAlbums').html(albumHTML);
 
+  // ___________button to return to default home page_________
+  $('nav').on('click', '.home', function(event) {
+      event.preventDefault();
+      console.log(this);
+
+    var albumTemplate = _.template($('#albumTmpl').html());
+      var albumHTML = "";
+
+      // we need to merge our data from albumData with actual markup so that we can add to our html document
+      _.each(albumData, function (currVal, idx, arr) {
+        albumHTML += albumTemplate(currVal);
+      });
+      $('.allAlbums').html(albumHTML);
+      $('.allAlbums').addClass('active-section');
+      $('.picture1').removeClass('active-section');
+    });
+
 // --------------make it look like multiple pages-----------------
 // when you click the button in the sidebar
-$('nav button').on('click', function(event) {
+$('nav').on('click', 'button', function(event) {
     event.preventDefault();
-
+      console.log(clickedSection);
       var clickedSection = "." + $(this).find('a').attr('rel');
       var correctAlbum = albumData.filter(function(currItem){
         return currItem.name === clickedSection.split(".")[1];
@@ -33,7 +50,7 @@ $('nav button').on('click', function(event) {
       // we need to merge our data from pictureData with actual markup so that we can add to our html document
       pictureHTML += pictureTemplate(correctAlbum[0]);
       $('.picture1').html(pictureHTML);
-      $(clickedSection).addClass('active-section');
+      $('.picture1').addClass('active-section');
       $('.allAlbums').removeClass('active-section');
       // $('.allAlbums').toggle('active-section');
 // can't figure out how to get clicked images to be hidden when I toggle - it shows all albums and the images inside the clicked button
@@ -41,5 +58,24 @@ $('nav button').on('click', function(event) {
       $('header').html(photoBy);
 
     });
+// ________click on image of album___________________
+    $('.allAlbums').on('click', '.album', function(event) {
+        event.preventDefault();
+          var clickedSection= $(this).children('h1').text();
+          var correctAlbum = albumData.filter(function(currItem){
+            return currItem.name === clickedSection;
+          });
+          var pictureTemplate = _.template($('#pictureTmpl').html());
+          var pictureHTML = "";
+
+          _.each(correctAlbum, function (currItem) {
+            pictureHTML += pictureTemplate(currItem);
+          });
+
+          $('.picture1').html(pictureHTML);
+          $('.picture1').addClass('active-section');
+          $('.allAlbums').removeClass('active-section');
+
+        });
 
 });
